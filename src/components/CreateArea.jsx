0a6b480fce88e9b2a62a7d66f5760cc5af6bf710
Daniel Ogbuti <danielogbuti@gmail.com>
2020-11-10 13:57:03 +0100

    setup create area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,18 +1,49 @@
 import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   const [fullText, setFullText] = useState({
     title: "",
     content: ""
   });
 
-  function handleChange(event) {}
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    console.log(value);
+
+    setFullText({
+      ...fullText,
+      [name]: value
+    });
+  }
+
+  function handleSubmit(event) {
+    console.log(fullText);
+
+    props.onAdd(fullText);
+    setFullText({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          onChange={handleChange}
+          placeholder="Title"
+          value={fullText.title}
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          placeholder="Take a note..."
+          rows="3"
+          value={fullText.content}
+        />
         <button>Add</button>
       </form>
     </div>
