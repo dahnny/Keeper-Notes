55488ed2b024ee69d399f09102d634caaf8adc75
Daniel Ogbuti <danielogbuti@gmail.com>
2020-11-10 14:36:45 +0100

    setup notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function AddItem(note) {
+    let noteItems = [...notes];
+    noteItems.push(note);
+    setNotes(noteItems);
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={AddItem} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
